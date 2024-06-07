a35945434594d1995113bd287ae821894719939a
jsangalang93 <jsangalang93@gmail.com>
2024-06-07 11:20:09 -0400

    through migrations

--- instruments_api/models.py
+++ instruments_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Instrument(models.Model):
+    name = models.CharField(max_length=32)
+    category = models.CharField(max_length=32)
+    brand = models.CharField(max_length=32)
+    model = models.CharField(max_length=32)
+    condition = models.CharField(max_length=32)
+    isRented = models.BooleanField(default=False)
+    # rentalStart = models.DateTimeField(auto_now_add=True)
+    # rentalEnd = models.DateTimeField(auto_now=True)
+   
