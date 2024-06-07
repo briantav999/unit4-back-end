e4d63b3e914f120274fab5a01ea2d1e0f9b16ef9
jsangalang93 <jsangalang93@gmail.com>
2024-06-07 12:29:10 -0400

    double checked, works

--- instruments_api/urls.py
+++ instruments_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/instruments', views.InstrumentList.as_view(), name='instrument_list'),
+    path('api/instruments/', views.InstrumentList.as_view(), name='instrument_list'),
     path('api/instruments/<int:pk>', views.InstrumentDetail.as_view(), name='instrument_detail'),
 ]
